1a148b94850e38d2b1485326523b782e51b45373
maotou <heihei@163.com>
2017-11-21 09:40:13 +0800

    添加控件java包

--- app/src/main/java/widgets/MZModeBannerFragment.java
+++ app/src/main/java/widgets/MZModeBannerFragment.java
@@ -1,4 +1,4 @@
-package com.yxl.shishile.shishile;
+package widgets;
 
 /**
  * Created by Administrator on 2017/11/21 0021.
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.yxl.shishile.shishile.R;
