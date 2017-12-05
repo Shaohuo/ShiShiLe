52e578de52b39c08c8738dc0deb5120b4ebbb507
John Doe <605750565@qq.com>
2017-12-05 14:40:27 +0800

    增加充值页面点击事件

--- app/src/main/java/com/yxl/shishile/shishile/activity/BPActivity.java
+++ app/src/main/java/com/yxl/shishile/shishile/activity/BPActivity.java
@@ -3,8 +3,10 @@ package com.yxl.shishile.shishile.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yxl.shishile.shishile.R;
 
@@ -19,6 +21,8 @@ public class BPActivity extends AppCompatActivity implements View.OnClickListene
         RadioButton bp_r1 = findViewById(R.id.bp_radio_1);
         RadioButton bp_r2 = findViewById(R.id.bp_radio_2);
         RadioButton bp_r3 = findViewById(R.id.bp_radio_3);
+        Button weixin_pay = findViewById(R.id.weixin_pay);
+        Button zhifu_pay = findViewById(R.id.zhifu_pay);
         money_num = findViewById(R.id.money_num);
         bp_r1.setOnClickListener(this);
         bp_r2.setOnClickListener(this);
@@ -39,6 +43,12 @@ public class BPActivity extends AppCompatActivity implements View.OnClickListene
                 break;
             case R.id.bp_radio_3:
                 money_num.setText("50元");
+                break;
+            case R.id.weixin_pay:
+                Toast.makeText(this, "微信支付~马上开放", Toast.LENGTH_SHORT);
+                break;
+            case R.id.zhifu_pay:
+                Toast.makeText(this, "支付宝支付~马上开放", Toast.LENGTH_SHORT);
                 break;
         }
 
