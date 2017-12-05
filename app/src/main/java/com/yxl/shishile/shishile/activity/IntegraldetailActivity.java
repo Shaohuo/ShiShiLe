f244e61974f4c57351aa2775571431c2c9d95e47
John Doe <605750565@qq.com>
2017-12-05 15:45:15 +0800

    增加我的积分界面

---
+++ app/src/main/java/com/yxl/shishile/shishile/activity/IntegraldetailActivity.java
@@ -0,0 +1,15 @@
+package com.yxl.shishile.shishile.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yxl.shishile.shishile.R;
+
+public class IntegraldetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_integraldetail);
+    }
+}
