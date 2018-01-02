fe1425e732123d5a716eca318b7ed496447cfa5a
shiyid <shiyid@example.com>
2018-01-02 20:12:11 +0800

    完善个人中心页面功能

---
+++ app/src/main/java/com/yxl/shishile/shishile/activity/MineActivity.java
@@ -0,0 +1,28 @@
+package com.yxl.shishile.shishile.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.yxl.shishile.shishile.R;
+
+public class MineActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+        ImageView backImg = findViewById(R.id.back_img);
+        backImg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.back_img:
+                finish();
+                break;
+        }
+    }
+}
